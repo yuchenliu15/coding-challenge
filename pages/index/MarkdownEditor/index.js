5ba094a7db4486fc1f17f4469e1ad77510044476
Yuchen Liu <yl6448@nyu.edu>
2020-05-22 20:12:23 -0700

    markdown auto save

--- pages/index/MarkdownEditor/index.js
+++ pages/index/MarkdownEditor/index.js
@@ -5,23 +5,18 @@ import rdmd from '@readme/markdown';
 
 function MarkdownEditor({ file, write}) {
 
-
   const [content, setContent] = React.useState('');
+  const [time, setTime] = React.useState(null);
+  const [save, setSave] = React.useState(true);
 
   useEffect(() => {
     (async () => {
       setContent(await file.text());
+      setSave(true);
     })();
   }, [file]);
 
-  const onFileChange = (event) => {
-    const input = event.target.value;
-    setContent(input);
-
-  }
-
-  const onExit = (event) => {
-    const input = event.target.value;
+  const updateFiles = (input) => {
     const newFile = new File(
       [input],
       file.name,
@@ -33,9 +28,28 @@ function MarkdownEditor({ file, write}) {
     write(newFile);
   }
 
+  const onFileChange = (event) => {
+    const input = event.target.value;
+    setContent(input);
+    clearTimeout(time);
+    setTime(setTimeout(() => {
+      updateFiles(input);
+    }, 2000));
+    if(save)
+      setSave(false);
+  }
+
+  const onExit = (event) => {
+
+    clearTimeout(time);
+
+    const input = event.target.value;
+    updateFiles(input);
+  }
   return (
     <div>
       <h3>{file.name}</h3>
+      <p style={{'color':'blue'}}>{save? 'Saved!': 'Writing...'}</p>
       <textarea className={css.editor} value={content} rows={20} onChange={onFileChange} onBlur={onExit}></textarea>
 
       <div className={css.preview}>
